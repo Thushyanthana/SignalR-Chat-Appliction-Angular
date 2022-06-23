6ced19bbfd86dde7cca34855aad5ef93766cf306
unknown <thushiravirajan01@gmail.com>
2022-06-23 07:38:12 +0530

    Initial Commit Client Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+import {MessageDto } from 'src/app/Dto/MessageDto';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-chat-app';
+  constructor(private chatService: ChatService) {
+  }
+
+  ngOnInit(): void {
+    this.chatService.retrieveMappedObject().subscribe( (receivedObj: MessageDto) => { this.addToInbox(receivedObj);});  // calls the service method to get the new messages sent
+                                                     
+  }
+
+  msgDto: MessageDto = new MessageDto();
+  msgInboxArray: MessageDto[] = [];
+
+  chatuser=this.msgDto.user;
+
+  setUsername()
+  {
+ this.chatuser = this.msgDto.user;
+if(this.chatuser===null)
+{
+alert("please enter the User name");
+}
+
+  }
+
+  send(): void {
+    if(this.msgDto) {
+      if(this.msgDto.user.length == 0 || this.msgDto.user.length == 0){
+        window.alert("fields are required.");
+        return;
+      } else {
+        this.chatService.broadcastMessage(this.msgDto).subscribe(data => {
+          this.msgDto.msgText = '';
+        }); 
+                          // Send the message via a service
+}
+    }
+  }
+
+  addToInbox(obj: MessageDto) {
+    let newObj = new MessageDto();
+    newObj.user = obj.user;
+    newObj.msgText = obj.msgText;
+    this.msgInboxArray.push(newObj);
+  //  this.msgDto.user=" ";
+  // this.msgDto.msgText=" ";
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  hubConnectionURL: 'https://localhost:44368/chatsocket',
+  broadcastURL: 'https://localhost:44368/api/chat/send'
 };
 
 /*
